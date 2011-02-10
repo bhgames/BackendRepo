6501cb78c9b212eda923168b11fb9fffbd75063a
Jordan Prince <jordanmprince@gmail.com>
2011-02-10 00:36:34 -0600

    Added bid to building displays

--- BHEngine/Controllers.java
+++ BHEngine/Controllers.java
@@ -1371,6 +1371,8 @@ public boolean noFlick(HttpServletRequest req, PrintWriter out) {
 			        	.value(b.getLvl())
 			        	.key("lotNum")
 			        	.value(b.getLotNum())
+			        	.key("bid")
+			        	.value(b.getBid())
 			        	.key("refuelTicks")
 			        	.value(b.getRefuelTicks())
 			        	.key("ticksToFinish")
